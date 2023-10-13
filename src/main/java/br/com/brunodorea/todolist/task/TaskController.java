0fe132fe0050bc7fb7d456454256578372149049
Bruno Henrique <brunodorea@outlook.com.br>
2023-10-13 08:35:52 -0300

    :sparkles: Add Owner User Validation

--- src/main/java/br/com/brunodorea/todolist/task/TaskController.java
+++ src/main/java/br/com/brunodorea/todolist/task/TaskController.java
@@ -51,10 +51,21 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var task = this.taskRepository.findById(id).orElse(null);
+        var idUser = request.getAttribute("idUser");
+        
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada.");
+        }
+
+        if(!task.getIdUser().equals(idUser)) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alteração desta tarefa.");
+            }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdate = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdate));
     }
 }
